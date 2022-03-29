413f32eda5aa011711f19bd93879e0cba7db17bd
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-29 17:27:19 -0300

    added files by upload

---
+++ game.py
@@ -0,0 +1,13 @@
+separator = "                                               "
+welcome_text = """
+The village of Honeywood has been terrorized by strange, deadly creatures for months now. Unable to endure any 
+longer, the villagers pooled their wealth and hired the most skilled adventurer they could find: you. After
+listening to their tale of woe, you agree to enter the labyrinth where most of the creatures seem to originate,
+and destroy the foul beasts. Armed with a longsword and a bundle of torches, you descend into the labyrinth, 
+ready to do battle....""" 
+
+
+def welcome():
+    print(separator, "DUNGEON")
+    print(welcome_text)
+
---
+++ main.py
@@ -0,0 +1,11 @@
+import game
+
+def main():
+    # print welcome screen
+    game.welcome()
+
+
+
+if __name__ == "__main__":
+    main()
+
